422b29eb62641eedc05c093154005cca92886add
Maria Milagros Jaramillo Rios <mariwrios@gmail.com>
2021-07-15 04:29:58 -0400

    creacion e implementacion de hook useForm en el login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 import './login.css'
 
 export const Login = () => {
+  const initialForm ={
+    email: '',
+    password:''
+  }
+  const [formValues, handleInputChange, reset] = useForm(initialForm)
+
+  const {email, password} = formValues;
+
+  const hadleInputSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues)
+  }
+
   return (
     <div className='login__container'>
       <img src='./assets/auth/logo.svg'/>
-      <form>
+
+      <form onSubmit={hadleInputSubmit}>
         
-        <input placeholder='Escriba su correo'/>
+        <input placeholder='Escriba su correo' name='email' value={email} onChange={handleInputChange} type='email' autoComplete='off'/>
         
-        <input placeholder='Escriba su contraseña'/>
+        <input placeholder='Escriba su contraseña' name='password' value={password} onChange={handleInputChange} type='password' autoComplete='off'/>
         <button>Iniciar Sesión</button>
       </form>
 
