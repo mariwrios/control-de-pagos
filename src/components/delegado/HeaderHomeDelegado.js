fb11a3ff267cd4ebd4ad7ec24a1a25574ba8eaa1
Maria Milagros Jaramillo Rios <mariwrios@gmail.com>
2021-07-15 06:38:54 -0400

    componente header home delegado

---
+++ src/components/delegado/HeaderHomeDelegado.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const HeaderHomeDelegado = () => {
+  return (
+    <div className='delegado-home_hero'>
+    <div className='delegado-home_hero--header'>
+      <img src='./assets/delegado/icon-arrow-left.svg'/>
+      <span>Delegado</span>
+    </div>
+    <h2>Lista de Vecinos</h2>
+    <div className='delegado-home_hero--subtitle'>
+      <img src='./assets/delegado/icon-direction.svg'/>
+      <h4>Tulipan, Calle #12 </h4>
+    </div>
+    <div className='delegado-home__buscador--container'>
+    <input/>
+    <img src='./assets/delegado/icon-lupa.svg'/>
+    </div>
+  
+  </div>
+  )
+}
--- src/components/delegado/HomeDelegado.js
+++ src/components/delegado/HomeDelegado.js
@@ -1,25 +1,11 @@
 import React from 'react'
+import { HeaderHomeDelegado } from './HeaderHomeDelegado'
 import './home-delegado.css'
 
 export const HomeDelegado = () => {
   return (
     <div className='delegado-home_container'>
-      <div className='delegado-home_hero'>
-        <div className='delegado-home_hero--header'>
-          <img src='./assets/delegado/icon-arrow-left.svg'/>
-          <span>Delegado</span>
-        </div>
-        <h2>Lista de Vecinos</h2>
-        <div className='delegado-home_hero--subtitle'>
-          <img src='./assets/delegado/icon-direction.svg'/>
-          <h4>Tulipan, Calle #12 </h4>
-        </div>
-        <div className='delegado-home__buscador--container'>
-        <input/>
-        <img src='./assets/delegado/icon-lupa.svg'/>
-        </div>
-      
-      </div>
+   <HeaderHomeDelegado/>
       
     </div>
   )
